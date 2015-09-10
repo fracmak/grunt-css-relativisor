ce4025967ad9d956805dc483e24f9555bd54ebe0
Merrifield, Jay <fracmak@gmail.com>
2015-09-10 15:13:33 -0400

    Complete rewrite of the relativisor. It now can do more than just rewrite files in place, you can now specify a destination directory, as well as expanding the relativisor to handle relative urls and rebasing them. This also fixes a bug with absolute paths being garbled. Options have been rewritten to make them clearer on what they do

---
+++ Gruntfile.js
@@ -0,0 +1,51 @@
+'use strict';
+
+module.exports = function(grunt) {
+    // Project Configuration
+    grunt.initConfig({
+        jshint: {
+            all: [
+                'Gruntfile.js',
+                'tasks/**/*.js',
+                'tests/**/*.js'
+            ],
+            options: {
+                jshintrc: '.jshintrc'
+            }
+        },
+        clean: {
+            tests: ['tmp']
+        },
+        'css-relativisor': {
+            default: {
+                files: {
+                    'tmp/sample_default.css': ['tests/fixtures/sample.css']
+                }
+            },
+            root: {
+                options: {
+                    root: 'tests'
+                },
+                files: {
+                    'tmp/sample_root.css': ['tests/fixtures/sample.css']
+                }
+            },
+            norebase: {
+                options: {
+                    rebase: false
+                },
+                files: {
+                    'tmp/sample_norebase.css': ['tests/fixtures/sample.css']
+                }
+            }
+        },
+        nodeunit: {
+            tests: ['tests/*.test.js']
+        }
+    });
+
+    // Load NPM tasks
+    require('load-grunt-tasks')(grunt);
+    grunt.loadTasks('tasks');
+    grunt.registerTask('test', ['clean', 'jshint', 'css-relativisor', 'nodeunit']);
+};
